d9b76dabd234d0760628cd2757c913b62280ec07
pauloosilas <paulo3356@gmail.com>
2016-03-17 19:40:35 -0300

    Loja virtual MVC 5

---
+++ Quirom.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quirom.LojaVirtual.Dominio.Entidade
+{
+    class Produto
+    {
+        public int ProdutoId { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+        public String Categorial { get; set; }
+    }
+}
---
+++ Quirom.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Quirom.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestQuirom
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
